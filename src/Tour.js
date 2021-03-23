dd9cdf1cc32a974e6711bb526aa786923c8074dc
codeKingMike <codekingmike@gmail.com>
2021-03-22 21:10:21 -0700

    making a tour app

---
+++ src/Tour.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+
+const Tour = ({ id, name, info, image, price }) => {
+    return <article className="single-tour">
+        <img src={image} alt="" />
+        <footer>
+            <div className="tour-info">
+                <h4>{name}</h4>
+                <h4 className="tour-price">${price}</h4>
+            </div>
+        </footer>
+    </article>
+};
+
+export default Tour;
